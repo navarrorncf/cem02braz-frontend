c1c14a206c4690602b2b3ac5da0a1894d130edf3
Rafael Navarro C. Filho <rncf1112@gmail.com>
2021-06-12 13:03:59 -0300

    Added Montserrat font and basic resets

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@font-face {
+  font-family: "Montserrat";
+  src: url("./assets/fonts/Montserrat-VF.woff2");
 }
 
-#nav {
-  padding: 30px;
+html {
+  font-size: 62.5%;
+  scroll-behavior: smooth;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+body {
+  box-sizing: border-box;
+  font-size: 1.6rem;
+  font-family: "Montserrat", Arial, Helvetica, sans-serif;
+  font-variation-settings: "wght" 400;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
 }
 </style>
