541d9513105cad0955dfafb3a62f75c817ec8096
Rafael Navarro C. Filho <rncf1112@gmail.com>
2021-06-13 22:04:59 -0300

    Added boilerplate Section elements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,65 @@
 <template>
   <Header />
+  <Section id="first-section" title="Um Pouco Sobre Nós">
+    <p class="text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. In suscipit,
+      incidunt quisquam autem, harum sequi, eius perspiciatis quis enim
+      voluptatem placeat voluptates quos repellendus molestias explicabo?
+      Dolore, voluptates quas reiciendis expedita quae impedit accusantium,
+      repudiandae officiis cupiditate itaque natus porro.
+    </p>
+    <p class="text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic omnis non in
+      facilis eligendi laboriosam voluptatibus odio porro adipisci quam, minima
+      qui dolorem maiores repudiandae molestiae ea modi asperiores ut et
+      possimus minus officiis. Ullam, reiciendis doloribus in quia tenetur sunt
+      ipsam consectetur, similique aut id magnam? Omnis, reprehenderit dolor.
+    </p>
+  </Section>
+  <Section title="Nossos Valores">
+    <p class="text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. In suscipit,
+      incidunt quisquam autem, harum sequi, eius perspiciatis quis enim
+      voluptatem placeat voluptates quos repellendus molestias explicabo?
+      Dolore, voluptates quas reiciendis expedita quae impedit accusantium,
+      repudiandae officiis cupiditate itaque natus porro.
+    </p>
+    <p class="text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic omnis non in
+      facilis eligendi laboriosam voluptatibus odio porro adipisci quam, minima
+      qui dolorem maiores repudiandae molestiae ea modi asperiores ut et
+      possimus minus officiis. Ullam, reiciendis doloribus in quia tenetur sunt
+      ipsam consectetur, similique aut id magnam? Omnis, reprehenderit dolor.
+    </p>
+  </Section>
+  <Section title="Venha Conhecer">
+    <p class="text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. In suscipit,
+      incidunt quisquam autem, harum sequi, eius perspiciatis quis enim
+      voluptatem placeat voluptates quos repellendus molestias explicabo?
+      Dolore, voluptates quas reiciendis expedita quae impedit accusantium,
+      repudiandae officiis cupiditate itaque natus porro.
+    </p>
+    <p class="text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic omnis non in
+      facilis eligendi laboriosam voluptatibus odio porro adipisci quam, minima
+      qui dolorem maiores repudiandae molestiae ea modi asperiores ut et
+      possimus minus officiis. Ullam, reiciendis doloribus in quia tenetur sunt
+      ipsam consectetur, similique aut id magnam? Omnis, reprehenderit dolor.
+    </p>
+  </Section>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from "@/components/header/Header";
+import Section from "@/components/Section";
 
 export default {
   name: "Home",
   components: {
     Header,
+    Section,
   },
 };
 </script>
