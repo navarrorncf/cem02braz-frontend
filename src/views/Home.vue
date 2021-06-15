2cef10fa7c6d3f75885a9a44fe42449ad77906f4
Rafael Navarro C. Filho <rncf1112@gmail.com>
2021-06-15 19:27:29 -0300

    added Contact, Location and Values components
    removed previous boilerplate code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <Header />
-  <Section id="first-section" title="Um Pouco Sobre Nós">
+  <Section
+    id="first-section"
+    title="Um Pouco Sobre Nós"
+    fontAwesomeIconClasses="fa fa-history"
+  >
     <p class="text">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. In suscipit,
       incidunt quisquam autem, harum sequi, eius perspiciatis quis enim
@@ -16,7 +20,7 @@
       ipsam consectetur, similique aut id magnam? Omnis, reprehenderit dolor.
     </p>
   </Section>
-  <Section title="Nossos Valores">
+  <Section title="Nossos Valores" fontAwesomeIconClasses="far fa-gem">
     <p class="text">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. In suscipit,
       incidunt quisquam autem, harum sequi, eius perspiciatis quis enim
@@ -24,44 +28,30 @@
       Dolore, voluptates quas reiciendis expedita quae impedit accusantium,
       repudiandae officiis cupiditate itaque natus porro.
     </p>
-    <p class="text">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic omnis non in
-      facilis eligendi laboriosam voluptatibus odio porro adipisci quam, minima
-      qui dolorem maiores repudiandae molestiae ea modi asperiores ut et
-      possimus minus officiis. Ullam, reiciendis doloribus in quia tenetur sunt
-      ipsam consectetur, similique aut id magnam? Omnis, reprehenderit dolor.
-    </p>
+    <Values />
   </Section>
-  <Section title="Venha Conhecer">
-    <p class="text">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. In suscipit,
-      incidunt quisquam autem, harum sequi, eius perspiciatis quis enim
-      voluptatem placeat voluptates quos repellendus molestias explicabo?
-      Dolore, voluptates quas reiciendis expedita quae impedit accusantium,
-      repudiandae officiis cupiditate itaque natus porro.
-    </p>
-    <p class="text">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic omnis non in
-      facilis eligendi laboriosam voluptatibus odio porro adipisci quam, minima
-      qui dolorem maiores repudiandae molestiae ea modi asperiores ut et
-      possimus minus officiis. Ullam, reiciendis doloribus in quia tenetur sunt
-      ipsam consectetur, similique aut id magnam? Omnis, reprehenderit dolor.
-    </p>
-  </Section>
+  <Contact />
+  <Location />
   <Footer />
 </template>
 
 <script>
 // @ is an alias to /src
+import Contact from "@/components/Contact";
 import Header from "@/components/header/Header";
-import Section from "@/components/Section";
 import Footer from "@/components/Footer";
+import Location from "@/components/Location";
+import Section from "@/components/Section";
+import Values from "@/components/Values";
 
 export default {
   name: "Home",
   components: {
     Header,
     Section,
+    Values,
+    Contact,
+    Location,
     Footer,
   },
 };
