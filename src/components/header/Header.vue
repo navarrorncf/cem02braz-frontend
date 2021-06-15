6363c4a7e2fd5482b4fc0d89c03b68d6c52396c2
Rafael Navarro C. Filho <rncf1112@gmail.com>
2021-06-15 19:13:51 -0300

    removed width that caused overflow

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -21,7 +21,6 @@ export default {
 <style>
 header.hero-header {
   height: 100vh;
-  width: 100vw;
   background: linear-gradient(to left, #f1eade88, #f1eade88),
     url("../../assets/images/books.jpg") center center / cover;
   overflow: hidden;
