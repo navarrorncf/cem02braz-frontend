850cca185b4cee2497b1dcaa02d2edee8ba61d73
Rafael Navarro C. Filho <rncf1112@gmail.com>
2021-06-15 19:23:51 -0300

    added fa icons to title and bg pattern to section
    added text-shadow to h3 and lowered font weight
    changed root div to a semantic 'section' element

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="section">
+  <section class="section">
     <div class="section--content">
-      <h2 class="title">{{ title }}</h2>
+      <h2 class="title">
+        {{ title }}
+        <i v-if="fontAwesomeIconClasses" :class="fontAwesomeIconClasses"></i>
+      </h2>
       <slot></slot>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -12,6 +15,10 @@ export default {
   name: "Section",
   props: {
     title: String,
+    fontAwesomeIconClasses: {
+      type: String,
+      default: "",
+    },
   },
 };
 </script>
@@ -22,25 +29,38 @@ export default {
   color: #5d5246;
 }
 
-.section:nth-of-type(even) {
-  background: #f5f3ef;
+.section:nth-of-type(odd) {
+  background: linear-gradient(to top, #e4e2de66, #e4e2de66),
+    url("../assets/images/papyrus.png");
+  background-attachment: fixed;
 }
 
 .section--content {
-  max-width: 60rem;
+  max-width: 70rem;
   margin: 0 auto;
   padding: 6rem 0;
 }
 
 .title {
   font-size: 4.2rem;
-  font-variation-settings: "wght" 550;
+  font-variation-settings: "wght" 375;
   margin-bottom: 4.2rem;
   font-variant: small-caps;
   letter-spacing: -1px;
+  text-shadow: 0 2px 2px #5d5246;
 }
 
 .text {
   max-width: 30em;
+}
+
+.title i {
+  color: #7c6e4c;
+  text-shadow: 0 2px 2px #5d5246;
+  margin-left: 1rem;
+}
+
+.title i.light-red {
+  color: #e23232;
 }
 </style>
